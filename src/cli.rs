c840c0067d2f91d246df90d5ebfd15c350e9d515
show-t <show-t@users.noreply.github.com>
2025-10-02 01:25:56 +0900

    [docs] CLIコマンドのヘルプテキストの追加

--- src/cli.rs
+++ src/cli.rs
@@ -8,17 +8,23 @@ pub(crate) struct Args {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
+    /// Automatically generate directories and files from a YAML file  
     Create {
+        /// Specify the directory structure definition file
         #[arg(short, long, default_value = "dirmap.yaml")]
         input: String,
         
+        ///Specify the directory structure to extract to
         #[arg(short, long, default_value = ".")]
         output: String,
     },
+    /// Export directory structures to a YAML file  
     Export {
+        /// Specify the directory to export
         #[arg(short, long, default_value = ".")]
         target: String,
         
+        /// Specify output file
         #[arg(short, long, default_value = "dirmap.yaml")]
         output: String,
     }
